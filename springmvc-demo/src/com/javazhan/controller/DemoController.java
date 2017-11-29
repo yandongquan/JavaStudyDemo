1e52a5875c71fce6ffbd96fdaac5c865e2743a6d
yandongquan <yandongquanlove@gmail.com>
2017-11-29 17:23:45 +0800

     :gift_heart: add springmvc-demo

---
+++ springmvc-demo/src/com/javazhan/controller/DemoController.java
@@ -0,0 +1,20 @@
+package com.javazhan.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Created by yando on 2017/11/29.
+ */
+@Controller
+public class DemoController {
+    @RequestMapping("/demo")
+    public ModelAndView view(HttpServletRequest request){
+        ModelAndView mav = new ModelAndView();
+        mav.setViewName("index");
+        return mav;
+    }
+}
