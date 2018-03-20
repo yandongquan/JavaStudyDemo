2d49ecf7c96943499e69f763994d5399da41b129
yandongquan <yandongquanlove@gmail.com>
2018-03-20 17:36:28 +0800

    :joy: update activemq-demo

--- activemq-demo/src/test/java/com/javakz/ProducerTest.java
+++ activemq-demo/src/test/java/com/javakz/ProducerTest.java
@@ -14,6 +14,11 @@ import javax.jms.*;
  */
 public class ProducerTest {
 
+    /**
+     * 测试Queue
+     * 生产者：发送消息
+     * @throws JMSException
+     */
     @Test
     public void testQueueProducer() throws JMSException {
         // 1）创建 ConnectionFactory 对象，需要指定服务端 ip 及端口号。
@@ -31,8 +36,37 @@ public class ProducerTest {
         MessageProducer producer = session.createProducer(queue);
         // 7）创建一个 Message 对象，创建一个 TextMessage 对象。
         TextMessage textMessage = new ActiveMQTextMessage();
-        textMessage.setText("hello activeMq,this is my first test.");
+        textMessage.setText("hello activeMq,this is my first test-queue.");
 //        TextMessage textMessage = session.createTextMessage("hello activeMq,this is my first test.");
+        // 8）使用 Producer 对象发送消息。
+        producer.send(textMessage);
+        // 9）关闭资源。
+        producer.close();
+        session.close();
+        connection.close();
+    }
+
+    /**
+     * 测试Topic
+     * 生产者：发送消息
+     * @throws JMSException
+     */
+    @Test
+    public void testTopicProducer() throws JMSException {
+        // 1）创建 ConnectionFactory 对象，需要指定服务端 ip 及端口号。
+        ConnectionFactory connectionFactory = new ActiveMQConnectionFactory("tcp://192.168.0.113:61616");
+        // 2）使用 ConnectionFactory 对象创建一个 Connection 对象。
+        Connection connection = connectionFactory.createConnection();
+        // 3）开启连接，调用 Connection 对象的 start 方法。
+        connection.start();
+        // 4）使用 Connection 对象创建一个 Session 对象。
+        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+        // 5）使用 Session 对象创建一个 Destination 对象（topic、queue），此处创建一个 Topic对象。
+        Topic topic = session.createTopic("test-topic");
+        // 6）使用 Session 对象创建一个 Producer 对象。
+        MessageProducer producer = session.createProducer(topic);
+        // 7）创建一个 Message 对象，创建一个 TextMessage 对象。
+        TextMessage textMessage = session.createTextMessage("hello activeMq,this is my first test-topic.");
         // 8）使用 Producer 对象发送消息。
         producer.send(textMessage);
         // 9）关闭资源。
