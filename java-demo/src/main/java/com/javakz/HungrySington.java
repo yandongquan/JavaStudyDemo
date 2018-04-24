4f976710f72fbc365373c5497fc3be07484e6d3f
yandongquan <yandongquanlove@gmail.com>
2018-04-24 14:11:08 +0800

    :joy: add java-demo

---
+++ java-demo/src/main/java/com/javakz/HungrySington.java
@@ -0,0 +1,27 @@
+package com.javakz;
+
+/**
+ * @Author: yandq
+ * @Description: 单利模式-饿汉式
+ * @Date: Create in 10:19 2018/4/24
+ * @Modified By:
+ */
+public class HungrySington {
+    /**
+     * 直接创建对象
+     */
+    private static HungrySington hungrySington = new HungrySington();
+
+    /**
+     * 私有化构造方法
+     */
+    private HungrySington(){}
+
+    /**
+     * 返回对象实例
+     * @return 对象实例
+     */
+    public static HungrySington getHungrySington() {
+        return hungrySington;
+    }
+}
---
+++ java-demo/src/main/java/com/javakz/LazySingleton.java
@@ -0,0 +1,32 @@
+package com.javakz;
+
+/**
+ * @Author: yandq
+ * @Description:
+ * @Date: Create in 10:24 2018/4/24
+ * @Modified By:
+ */
+public class LazySingleton {
+    /**
+     * 声明变量
+     */
+    private static volatile LazySingleton lazySingleton = null;
+
+    /**
+     * 私有构造函数
+     */
+    private LazySingleton() {}
+
+    /**
+     *  提供对外方法
+     * @return 对象实例
+     */
+    public static LazySingleton getLazySingleton() {
+        if (lazySingleton==null) {
+            synchronized (LazySingleton.class) {
+                lazySingleton = new LazySingleton();
+            }
+        }
+        return lazySingleton;
+    }
+}
