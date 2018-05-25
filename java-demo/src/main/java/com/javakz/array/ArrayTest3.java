c175c9197087d433e996b1554978fb29cf0b79c8
yandongquan <yandongquanlove@gmail.com>
2018-05-25 18:20:17 +0800

    :joy: update java-demo

---
+++ java-demo/src/main/java/com/javakz/array/ArrayTest3.java
@@ -0,0 +1,21 @@
+package com.javakz.array;
+
+/**
+ * @Author: yandq
+ * @Description:
+ * @Date: Create in 18:19 2018/5/25
+ * @Modified By:
+ */
+public class ArrayTest3 {
+    public static void main(String[] args) {
+        int a[] = new int[2];
+        boolean [] b = new boolean[2];
+        String[] s = new String[2];
+        for(int i = 0; i < 2; i++)
+            System.out.println(a[i]);
+        for(int i = 0; i < 2; i++)
+            System.out.println(b[i]);
+        for(int i = 0; i < 2; i++)
+            System.out.println(s[i]);
+    }
+}
