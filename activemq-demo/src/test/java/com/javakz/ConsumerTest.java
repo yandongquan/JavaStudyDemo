e9c93bfb4c73170d3d2bdf90e78489bc4962c10b
yandongquan <yandongquanlove@gmail.com>
2018-07-18 14:34:19 +0800

    :joy: update activemq-demo

--- activemq-demo/src/test/java/com/javakz/ConsumerTest.java
+++ activemq-demo/src/test/java/com/javakz/ConsumerTest.java
@@ -23,7 +23,7 @@ public class ConsumerTest {
     @Test
     public void testQueueConsumer() throws JMSException, IOException {
         // 1）创建一个 ConnectionFactory 对象。
-        ConnectionFactory connectionFactory = new ActiveMQConnectionFactory("tcp://192.168.0.113:61616");
+        ConnectionFactory connectionFactory = new ActiveMQConnectionFactory("tcp://192.168.2.129:61616");
         // 2）从 ConnectionFactory 对象中获得一个 Connection 对象。
         Connection connection = connectionFactory.createConnection();
         // 3）开启连接。调用 Connection 对象的 start 方法。
@@ -65,9 +65,9 @@ public class ConsumerTest {
     @Test
     public void testTopicCusumer() throws JMSException, IOException {
         // 1）创建一个 ConnectionFactory 对象。
-        ConnectionFactory connectionFactory = new ActiveMQConnectionFactory("tcp://192.168.0.113:61616");
+        ConnectionFactory connectionFactory = new ActiveMQConnectionFactory("tcp://192.168.2.129:61616");
         // 2）从 ConnectionFactory 对象中获得一个 Connection 对象。参数用户名/密码
-        Connection connection = connectionFactory.createConnection("yan","yan");
+        Connection connection = connectionFactory.createConnection();
         // 3）开启连接。调用 Connection 对象的 start 方法。
         connection.start();
         // 4）使用 Connection 对象创建一个 Session 对象。
--- activemq-demo/src/test/java/com/javakz/ProducerTest.java
+++ activemq-demo/src/test/java/com/javakz/ProducerTest.java
@@ -23,14 +23,17 @@ public class ProducerTest {
     public void testQueueProducer() throws JMSException {
         // 1）创建 ConnectionFactory 对象，需要指定服务端 ip 及端口号。
         // brokerURL 服务器的IP及端口号
-        ConnectionFactory connectionFactory = new ActiveMQConnectionFactory("tcp://192.168.0.113:61616");
+        ConnectionFactory connectionFactory = new ActiveMQConnectionFactory("tcp://192.168.2.129:61616");
         // 2）使用 ConnectionFactory 对象创建一个 Connection 对象。
         Connection connection = connectionFactory.createConnection();
         // 3）开启连接，调用 Connection 对象的 start 方法。
         connection.start();
         // 4）使用 Connection 对象创建一个 Session 对象。
+        //第一个参数：是否开启事务。true：开启事务，第二个参数忽略。
+        //第二个参数：当第一个参数为false时，才有意义。消息的应答模式。1、自动应答2、手动应答。一般是自动应答。
         Session session = connection.createSession(false,Session.AUTO_ACKNOWLEDGE);
         // 5）使用 Session 对象创建一个 Destination 对象（topic、queue），此处创建一个 Queue对象。
+        // 参数：队列的名称。
         Queue queue = session.createQueue("test-queue");
         // 6）使用 Session 对象创建一个 Producer 对象。
         MessageProducer producer = session.createProducer(queue);
@@ -54,9 +57,9 @@ public class ProducerTest {
     @Test
     public void testTopicProducer() throws JMSException {
         // 1）创建 ConnectionFactory 对象，需要指定服务端 ip 及端口号。
-        ConnectionFactory connectionFactory = new ActiveMQConnectionFactory("tcp://192.168.0.113:61616");
+        ConnectionFactory connectionFactory = new ActiveMQConnectionFactory("tcp://192.168.2.129:61616");
         // 2）使用 ConnectionFactory 对象创建一个 Connection 对象。 参数用户名/密码
-        Connection connection = connectionFactory.createConnection("yan","yan");
+        Connection connection = connectionFactory.createConnection();
         // 3）开启连接，调用 Connection 对象的 start 方法。
         connection.start();
         // 4）使用 Connection 对象创建一个 Session 对象。
